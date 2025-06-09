8c84db34a4739c71223fc11e9ff61b60274c1e31
Jonathan Rankin <jonathan.rankin@mytutor.co.uk>
2025-06-09 21:59:07 +0100

    img

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -12,7 +12,14 @@ export default function HowItWorks({ id, title, backgroundColor, componentConten
               <h2>We license content from the world’s best reviewers</h2>
               <p>User reviews are important, but reviews from experienced experts are better. We have relationships with some of the most trusted publications in the world. We license their review content, and save it in our database. We are extremely selective. And no AI. This is real expertise from journalists who’ve testing products for decades.</p>
             </div>
-            <img id="how-it-works-image" src='/image1.png'></img>
+            <img id="how-it-works-image-1" src='image1.png'></img>
+          </div>
+          <div class="how-it-works-sub-section" id="how-section-2">
+            <img id="how-it-works-image-2" src='image2.png'></img>
+            <div>
+              <h2>We license content from the world’s best reviewers</h2>
+              <p>User reviews are important, but reviews from experienced experts are better. We have relationships with some of the most trusted publications in the world. We license their review content, and save it in our database. We are extremely selective. And no AI. This is real expertise from journalists who’ve testing products for decades.</p>
+            </div>
           </div>
         </div>
       </section>
