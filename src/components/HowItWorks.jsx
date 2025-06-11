281ebd3cc7b32ce58be32b4d1a91f4a065af73ec
Jonathan Rankin <jonathan.rankin@mytutor.co.uk>
2025-06-11 16:29:59 +0100

    looking food

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -9,7 +9,7 @@ export default function HowItWorks({ id, title, backgroundColor, componentConten
           </div>
 
           <div class="how-it-works-sub-section" id="how-section-1">
-            <div>
+            <div id="how-section-1-1">
               <h2>We license content from the world’s best reviewers</h2>
               <p>User reviews are important, but reviews from experienced experts are better. We have relationships with some of the most trusted publications in the world. We license their review content, and save it in our database. We are extremely selective. And no AI. This is real expertise from journalists who’ve testing products for decades.</p>
             </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,8 @@ export default function Navbar() {
     <nav id="navbar">
       <div id="navbar_content">
         <img id="navbar_logo_desktop" src="/reviewboat_logo_nav.svg" alt="ReviewBoat Logo" height="40" />
-        <div className="navbar-options">
+        <img id="navbar_logo_mobile" src="/reviewboat_logo.svg" alt="ReviewBoat Logo" height="40" />
+        <div id="navbar-options">
           <a href="#how-it-works">How it works</a>
           <a href="#publishers">Publishers</a>
           <a href="#retailers">Retailers</a>
