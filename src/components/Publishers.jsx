732f472f5a79b014f3882c9b7577d8705af5af51
jranks123 <jonnyrankin@googlemail.com>
2025-06-19 11:30:05 +0100

    styluing

--- src/components/Publishers.jsx
+++ src/components/Publishers.jsx
@@ -8,17 +8,17 @@ export default function Publishers({ id, title, backgroundColor, componentConten
         </div>
         <div class="icon-section">
           <div class="icon-subsection">
-            <img id="publishers-icon-1" src='/rbicon4.svg'></img>
+            <img class="pubreticon" id="publishers-icon-1" src='/rbicon4.svg'></img>
             <h3> Passive revenue stream </h3>
             <p> Earn from your existing content. We license and distribute your reviews—no effort required. </p>
           </div>
           <div class="icon-subsection">
-            <img id="publishers-icon-2" src='/rbicon5.svg'></img>
+            <img class="pubreticon" id="publishers-icon-2" src='/rbicon5.svg'></img>
             <h3> Beat algorithm changes </h3>
             <p> Bypass SEO and affiliate noise. Reach purchase-ready audiences directly at the point of sale. </p>
           </div>
           <div class="icon-subsection">
-            <img id="publishers-icon-2" src='/rbicon6.svg'></img>
+            <img class="pubreticon" id="publishers-icon-2" src='/rbicon6.svg'></img>
             <h3> Don’t rely on affiliate </h3>
             <p> Affiliate is unpredictable. Reviewboat offers consistent visibility and revenue - no cookies, no commissions </p>
           </div>
--- src/components/Retailers.jsx
+++ src/components/Retailers.jsx
@@ -8,17 +8,17 @@ export default function Retailers({ id, title, backgroundColor, componentContent
         </div>
         <div class="icon-section">
           <div class="icon-subsection">
-            <img id="retailers-icon-1" src='/rbicon1.svg'></img>
+            <img class="pubreticon" id="retailers-icon-1" src='/rbicon1.svg'></img>
             <h3> Avoid losing customers </h3>
             <p> Keep shoppers on your site with expert product reviews embedded directly on product pages. Reduce bounce and stop losing sales to Google. </p>
           </div>
           <div class="icon-subsection">
-            <img id="retailers-icon-2" src='/rbicon2.svg'></img>
+            <img class="pubreticon" id="retailers-icon-2" src='/rbicon2.svg'></img>
             <h3> Higher conversion rates </h3>
             <p> Trusted expert reviews increase buyer confidence and drive more purchases—typically boosting conversion rates by 10–25%. </p>
           </div>
           <div class="icon-subsection">
-            <img id="retailers-icon-2" src='/rbicon3.svg'></img>
+            <img class="pubreticon" id="retailers-icon-2" src='/rbicon3.svg'></img>
             <h3> No manual licensing </h3>
             <p> One line of code. No tagging, no chasing reviews. Content updates automatically across your catalogue. </p>
           </div>
