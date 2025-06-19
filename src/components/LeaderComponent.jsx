ee049cf951ea73b4ef65cbe8014795fd129e2afa
jranks123 <jonnyrankin@googlemail.com>
2025-06-19 10:44:23 +0100

    update

--- src/components/LeaderComponent.jsx
+++ src/components/LeaderComponent.jsx
@@ -5,7 +5,16 @@ export default function LeaderComponent({ id, title, backgroundColor, componentC
     <div class="section-container" style={{ backgroundColor }}>
       <section id={id} class="section" style={{ backgroundColor }}>
         <h2 class='extra-big-text'>Instant reviews from experts on product pages</h2>
-        <p>Get automatic reviews from experts on product pages. Close more sales.</p>
+        <div class="leader-subheading-container"> 
+          <div class="leader-subheading">          
+            <img id="pen-icon" class="leader-subheading-icon" src='/pen.svg'></img>
+            <p>Retailers close more sales.</p>
+          </div>
+          <div class="leader-subheading">          
+            <img id="shopping-icon" class="leader-subheading-icon" src='/shopping.svg'></img>
+            <p>Publishers sell more content.</p>
+          </div>
+        </div>
         <Button copy="Try Reviewboat" />
       </section>
     </div>
