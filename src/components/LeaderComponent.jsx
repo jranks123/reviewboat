4e1049cec9c56f8ef020cfe959e668601805ea30
jranks123 <jonnyrankin@googlemail.com>
2025-06-19 10:25:11 +0100

    update

--- src/components/LeaderComponent.jsx
+++ src/components/LeaderComponent.jsx
@@ -4,7 +4,7 @@ export default function LeaderComponent({ id, title, backgroundColor, componentC
   return (
     <div class="section-container" style={{ backgroundColor }}>
       <section id={id} class="section" style={{ backgroundColor }}>
-        <h2 class='extra-big-text'>Keep customers on your website</h2>
+        <h2 class='extra-big-text'>Instant reviews from experts on product pages</h2>
         <p>Get automatic reviews from experts on product pages. Close more sales.</p>
         <Button copy="Try Reviewboat" />
       </section>
